0b3d7f39e8d6e65f37e30d3e60e172e632dffafe
DIIAUS <77092023+DIIAUS@users.noreply.github.com>
2021-06-04 13:24:54 +0700

    W6

---
+++ Skill/Dead_Island.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int w, h;
+    cin >> w >> h;
+    int arr[w][h];
+    int count = 0;
+    for (int i=0;i<h;i++)
+    {
+        for (int j=0;j<w;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+
+    for (int i=0;i<h;i++)
+    {
+        for (int j=0;j<w;j++)
+        {
+            if (arr[i][j]==1)
+            {
+                if(arr[i-1][j-1]==1)
+                {count++;}
+                if(arr[i-1][j]==1)
+                {count++;}
+                if(arr[i-1][j+1]==1)
+                {count++;}
+                
+                
+                if(arr[i][j-1]==1)
+                {count++;}
+                if(arr[i][j]==1)
+                {count++;}
+                if(arr[i][j+1]==1)
+                {count++;}
+                
+
+                if(arr[i+1][j-1]==1)
+                {count++;}
+                if(arr[i+1][j]==1)
+                {count++;}
+                if(arr[i+1][j+1]==1)
+                {count++;}
+                
+
+                
+
+                
+                // for (int m=i-1;m<i+2;m++)
+                // {
+                //     for (int n=j-1; n<j+2;j++)
+                //     {
+                //         if (arr[m][n]==1)
+                //             ++count;
+                //     }
+                // }
+                if (count<3)
+                {
+                    cout<<"NO";
+                    return 0 ;
+                }
+            }
+        }
+    }
+    cout<<"YES";
+    return 0;
+}
